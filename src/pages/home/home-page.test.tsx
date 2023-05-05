934127d6c4171638aa27e3f0a8a42efaead115f8
Dmitriy Malakhov <mitia2022@gmail.com>
2023-05-05 17:03:26 +0700

    🔥 chore(home-page.test.tsx): remove eslint-disable and ts-nocheck comments

--- src/pages/home/home-page.test.tsx
+++ src/pages/home/home-page.test.tsx
@@ -1,3 +1,5 @@
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-nocheck
 import { test, describe, beforeAll, afterAll } from 'vitest';
 import { render, screen, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
